356aa920edd44d175dcdb92c348c0b989a968b9a
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2022-11-16 17:27:00 +0000

    Update log path

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,10 +8,10 @@ pack_paths:
   - /opt/flight/var/lib/pack/packs
 repo_paths:
   - /opt/flight/var/lib/pack/repos
-log_path: /opt/flight/var/log
+log_path: /opt/flight/var/log/pack
 store_dir: /opt/flight/var/cache/pack
 EOF
-mkdir -p /opt/flight/var/log
+mkdir -p /opt/flight/var/log/pack
 mkdir -p /opt/flight/var/cache/pack
 mkdir -p /opt/flight/var/lib/pack
 /opt/flight/bin/flight pack repoadd https://alces-flight-packs.s3.eu-west-2.amazonaws.com/v1/core.yml
